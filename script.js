89e9a9ead482b55b61135aacdbf1de86c49b4eec
Prazol Malla <prajwalmalla16@gmail.com>
2022-12-06 22:27:24 +0545

    arrow function

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ const appleJuice = fruitProcessor(5, 0);
 console.log(appleJuice);
 */
 
+/*
 function calcAge1(birthYear) {
   return 2037 - birthYear;
 }
@@ -38,3 +39,34 @@ const calcAge2 = function (birthYear) {
 
 const age2 = calcAge2(1992);
 console.log(age2);
+*/
+
+// Arrow function/
+/*
+const calcAge3 = (birthYear) => 2037 - birthYear;
+
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1991, "Bob"));
+*/
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+  console.log(apples, oranges);
+  const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));
